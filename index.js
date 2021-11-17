5ad56db7215897093c7b6f853a4276dfd5ada53d
Adriano Rama <ramaadriano@outlook.com>
2021-11-17 16:54:58 +0100

    commit on master

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const pricePerKm = 0.21;
 const age = Number(prompt("What is your age?"))
 const km = Number(prompt("How far are you traveling(km)?"))
 const ticketPrice = (km * pricePerKm);
-const juniorprice = (ticketPrice * 0.2)
-const elderprice = (ticketPrice * 0.4)
+const juniorprice = (ticketPrice * 0.8)
+const elderprice = (ticketPrice * 0.6)
 
 if(age < 18){
     alert(`You have won a 20% discount, Ticket Price: ${juniorprice.toFixed(2)} £`)
